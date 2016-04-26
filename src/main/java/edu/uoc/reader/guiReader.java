6250ac00d592a7b189065a07f7cb0a644add76c5
Manuel Velez Martinez <manuelvelez@gmail.com>
2016-04-26 23:22:29 +0200

    changes

--- src/main/java/edu/uoc/reader/guiReader.java
+++ src/main/java/edu/uoc/reader/guiReader.java
@@ -6,16 +6,23 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 /**
  * Created by mvelezm on 26/04/16.
  */
 public class guiReader {
 
+    private static String configFileName;
+    private static String odfFileName;
+
     guiReader() {
         showWindow();
     }
+    public void doTheConversion() {
 
+    }
     public static void showWindow() {
         JFrame frame = new JFrame("Demo application");
         frame.setSize(1000, 150);
@@ -50,6 +57,7 @@ public class guiReader {
                     File selectedFile = fileChooser.getSelectedFile();
                     System.out.println("Selected file: " + selectedFile.getAbsolutePath());
                     odfTextField.setText(selectedFile.getAbsolutePath());
+                    odfFileName = selectedFile.getAbsolutePath();
                 }
                 System.out.println("You clicked the button");
             }
@@ -61,12 +69,51 @@ public class guiReader {
         configLabel.setBounds(10, 40, 80, 25);
         panel.add(configLabel);
 
-        JTextField configTexField = new JPasswordField(20);
-        configTexField.setBounds(100, 40, 400, 25);
-        panel.add(configTexField);
+        final JTextField configTextField = new JTextField(200);
+        configTextField.setBounds(100, 40, 400, 25);
+        panel.add(configTextField);
 
-        JButton configButton = new JButton("Open");
+        final JButton configButton = new JButton("Open");
+        configButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+                int result = fileChooser.showOpenDialog(configButton);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = fileChooser.getSelectedFile();
+                    System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+                    configTextField.setText(selectedFile.getAbsolutePath());
+                    configFileName = selectedFile.getAbsolutePath();
+                }
+                System.out.println("You clicked the button");
+            }
+        });
         configButton.setBounds(500, 40, 80, 25);
         panel.add(configButton);
+
+        JButton startButton = new JButton("Start");
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                    Config setup = new Config(configFileName);
+                    ODTParser docParser;
+                    docParser = new ODTParser(odfFileName);
+                    String language = "ES";
+                    String text = docParser.getText();
+                    try {
+                        text = URLEncoder.encode(text, "utf-8");
+                    } catch (UnsupportedEncodingException ex) {
+                        ex.printStackTrace();
+                    }
+                    try {
+                        new espeakTTS().generateAudio(language, text);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                        System.out.println("[ERROR] Connection not available when online conversion selected");
+                        System.exit(1);
+                    }
+            }
+        });
+        startButton.setBounds(500, 70, 80, 25);
+        panel.add(startButton);
     }
 }

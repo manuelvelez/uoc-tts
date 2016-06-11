acb4dcaa4380b0478038da4420b62a8d6c733fd1
Manuel Velez Martinez <manuelvelez@gmail.com>
2016-06-11 14:15:24 +0200

    changes in text algorithm

--- src/main/java/edu/uoc/reader/ODPParser.java
+++ src/main/java/edu/uoc/reader/ODPParser.java
@@ -4,9 +4,17 @@ import org.apache.log4j.Logger;
 import org.apache.xerces.dom.TextImpl;
 import org.odftoolkit.odfdom.doc.OdfDocument;
 import org.odftoolkit.odfdom.dom.OdfContentDom;
+import org.odftoolkit.odfdom.dom.element.table.TableTableCellElement;
+import org.odftoolkit.odfdom.dom.element.table.TableTableElement;
+import org.odftoolkit.odfdom.dom.element.table.TableTableHeaderRowsElement;
+import org.odftoolkit.odfdom.dom.element.table.TableTableRowElement;
 import org.odftoolkit.odfdom.dom.element.text.TextSoftPageBreakElement;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Manuel Vélez on 14/04/16.
@@ -37,6 +45,51 @@ public class ODPParser extends ODFParser {
 
     private static final String PAGE_BREAK_MARK = "PAGE-BREAK-MARK";
 
+    //Static map with the word for table in each supported language
+    private static final Map<String, String> tableMap;
+    static {
+        Map<String, String> aMap = new HashMap<String, String>();
+        aMap.put("ES", "tabla, ");
+        aMap.put("EN", "table, ");
+        aMap.put("CA", "taula, ");
+        tableMap = Collections.unmodifiableMap(aMap);
+    }
+
+    //Static map with the word for row in each supported language
+    private static final Map<String, String> rowMap;
+    static {
+        Map<String, String> aMap = new HashMap<String, String>();
+        aMap.put("ES", "fila, ");
+        aMap.put("EN", "row, ");
+        aMap.put("CA", "fila, ");
+        rowMap = Collections.unmodifiableMap(aMap);
+    }
+
+    //Static map with the word for cell in each supported language
+    private static final Map<String, String> cellMap;
+    static {
+        Map<String, String> aMap = new HashMap<String, String>();
+        aMap.put("ES", "celda, ");
+        aMap.put("EN", "cell, ");
+        aMap.put("CA", "cel·la, ");
+        cellMap = Collections.unmodifiableMap(aMap);
+    }
+
+    //Static map with the word for header in each supported language
+    private static final Map<String, String> headerMap;
+    static {
+        Map<String, String> aMap = new HashMap<String, String>();
+        aMap.put("ES", "cabecera, ");
+        aMap.put("EN", "header, ");
+        aMap.put("CA", "capçalera, ");
+        headerMap = Collections.unmodifiableMap(aMap);
+    }
+
+    private String languageDependentTableName;
+    private String languageDependentRowName;
+    private String languageDependentCellName;
+    private String languageDependentHeaderName;
+
     @Override
     public String toString() {
 
@@ -50,6 +103,16 @@ public class ODPParser extends ODFParser {
         OdfContentDom content = null;
 
         content = document.getContentDom();
+
+        String documentLanguage = language;
+        if(!rowMap.containsKey(language))
+            documentLanguage="EN";
+
+        this.languageDependentTableName=tableMap.get(documentLanguage);
+        this.languageDependentRowName=rowMap.get(documentLanguage);
+        this.languageDependentCellName=cellMap.get(documentLanguage);
+        this.languageDependentHeaderName=headerMap.get(documentLanguage);
+
         NodeList list = content.getRootElement().getElementsByTagName("draw:page");
         String lastStyle = "";
 
@@ -60,22 +123,31 @@ public class ODPParser extends ODFParser {
     }
 
     public String recurse(Node node) {
-        String innerText = "";
+        StringBuilder innerText = new StringBuilder("");
         final String ENDLN = "\n";
 
         if (node instanceof TextImpl) {
-            innerText = innerText + node.getTextContent() + ENDLN;
+            innerText.append(node.getTextContent() + ENDLN);
         }
 
         else if(node instanceof TextSoftPageBreakElement) {
-            innerText = innerText + PAGE_BREAK_MARK;
+            innerText.append(PAGE_BREAK_MARK);
         }
+
+        else if (node instanceof TableTableElement)
+            innerText.append(this.languageDependentTableName);
+        else if (node instanceof TableTableHeaderRowsElement)
+            innerText.append(this.languageDependentHeaderName);
+        else if (node instanceof TableTableRowElement)
+            innerText.append(ENDLN + this.languageDependentRowName);
+        else if (node instanceof TableTableCellElement)
+            innerText.append(this.languageDependentCellName);
 
         if (node.hasChildNodes()) {
             for (int j = 0; j<node.getChildNodes().getLength(); j++){
-                innerText = innerText + this.recurse(node.getChildNodes().item(j));
+                innerText.append(this.recurse(node.getChildNodes().item(j)));
             }
         }
-        return innerText;
+        return innerText.toString();
     }
 }
--- src/main/java/edu/uoc/reader/ODSParser.java
+++ src/main/java/edu/uoc/reader/ODSParser.java
@@ -4,10 +4,18 @@ import org.apache.log4j.Logger;
 import org.apache.xerces.dom.TextImpl;
 import org.odftoolkit.odfdom.doc.OdfDocument;
 import org.odftoolkit.odfdom.dom.OdfContentDom;
+import org.odftoolkit.odfdom.dom.element.table.TableTableCellElement;
+import org.odftoolkit.odfdom.dom.element.table.TableTableElement;
+import org.odftoolkit.odfdom.dom.element.table.TableTableHeaderRowsElement;
+import org.odftoolkit.odfdom.dom.element.table.TableTableRowElement;
 import org.odftoolkit.odfdom.dom.element.text.TextSoftPageBreakElement;
 import org.odftoolkit.odfdom.incubator.doc.text.OdfTextParagraph;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Manuel Vélez on 14/04/16.
@@ -36,6 +44,51 @@ public class ODSParser extends ODFParser {
         this.text = text;
     }
 
+    //Static map with the word for table in each supported language
+    private static final Map<String, String> tableMap;
+    static {
+        Map<String, String> aMap = new HashMap<String, String>();
+        aMap.put("ES", "tabla, ");
+        aMap.put("EN", "table, ");
+        aMap.put("CA", "taula, ");
+        tableMap = Collections.unmodifiableMap(aMap);
+    }
+
+    //Static map with the word for row in each supported language
+    private static final Map<String, String> rowMap;
+    static {
+        Map<String, String> aMap = new HashMap<String, String>();
+        aMap.put("ES", "fila, ");
+        aMap.put("EN", "row, ");
+        aMap.put("CA", "fila, ");
+        rowMap = Collections.unmodifiableMap(aMap);
+    }
+
+    //Static map with the word for cell in each supported language
+    private static final Map<String, String> cellMap;
+    static {
+        Map<String, String> aMap = new HashMap<String, String>();
+        aMap.put("ES", "celda, ");
+        aMap.put("EN", "cell, ");
+        aMap.put("CA", "cel·la, ");
+        cellMap = Collections.unmodifiableMap(aMap);
+    }
+
+    //Static map with the word for header in each supported language
+    private static final Map<String, String> headerMap;
+    static {
+        Map<String, String> aMap = new HashMap<String, String>();
+        aMap.put("ES", "cabecera, ");
+        aMap.put("EN", "header, ");
+        aMap.put("CA", "capçalera, ");
+        headerMap = Collections.unmodifiableMap(aMap);
+    }
+
+    private String languageDependentTableName;
+    private String languageDependentRowName;
+    private String languageDependentCellName;
+    private String languageDependentHeaderName;
+
     @Override
     public String toString() {
 
@@ -50,42 +103,52 @@ public class ODSParser extends ODFParser {
         text="";
 
         content = document.getContentDom();
+
+        String documentLanguage = language;
+        if(!rowMap.containsKey(language))
+            documentLanguage="EN";
+
+        this.languageDependentTableName=tableMap.get(documentLanguage);
+        this.languageDependentRowName=rowMap.get(documentLanguage);
+        this.languageDependentCellName=cellMap.get(documentLanguage);
+        this.languageDependentHeaderName=headerMap.get(documentLanguage);
+
+        content = document.getContentDom();
         NodeList list = content.getRootElement().getElementsByTagName("office:spreadsheet");
         String lastStyle = "";
         for (int j = 0; j<list.item(0).getChildNodes().getLength(); j++){
             Node singleNode = list.item(0).getChildNodes().item(j);
-            if ( singleNode instanceof OdfTextParagraph){
-                if (lastStyle.equals("")){
-                    //First TextParagraph
-                    lastStyle = ((OdfTextParagraph) singleNode).getStyleName();
-                }
-                else if(!lastStyle.equals(((OdfTextParagraph) singleNode).getStyleName()))
-                    text = text + "PAGE-BREAK-MARK";
-            }
             text = text + this.recurse(list.item(0).getChildNodes().item(j));
-
         }
-
         textSplitted = this.text.replaceAll("(.{0,"+ 100+"})\\b", "$1\n").split("\n");
     }
 
     public String recurse(Node node) {
-        String innerText = "";
+        StringBuilder innerText = new StringBuilder("");
         final String ENDLN = "\n";
 
         if (node instanceof TextImpl) {
-            innerText = innerText + node.getTextContent() + ENDLN;
+            innerText.append(node.getTextContent() + ENDLN);
         }
 
         else if(node instanceof TextSoftPageBreakElement) {
-            innerText = innerText + "PAGE-BREAK-MARK";
+            innerText.append("PAGE-BREAK-MARK");
         }
+
+        else if (node instanceof TableTableElement)
+            innerText.append(this.languageDependentTableName);
+        else if (node instanceof TableTableHeaderRowsElement)
+            innerText.append(this.languageDependentHeaderName);
+        else if (node instanceof TableTableRowElement)
+            innerText.append(ENDLN + this.languageDependentRowName);
+        else if (node instanceof TableTableCellElement)
+            innerText.append(this.languageDependentCellName);
 
         if (node.hasChildNodes()) {
             for (int j = 0; j<node.getChildNodes().getLength(); j++){
-                innerText = innerText + this.recurse(node.getChildNodes().item(j));
+                innerText.append(this.recurse(node.getChildNodes().item(j)));
             }
         }
-        return innerText;
+        return innerText.toString();
     }
 }
